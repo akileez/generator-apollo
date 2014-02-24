4ec77a9b1366dda764622eb1f80c450be44208ca
akileez <git@keeda.net>
2014-02-24 01:15:31 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var shell = require('shelljs');
 
 
 var ApolloGenerator = yeoman.generators.Base.extend({
@@ -10,8 +11,28 @@ var ApolloGenerator = yeoman.generators.Base.extend({
     this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
 
     this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.npmInstall();
+      if (this.options['install']) {
+        shell.exec('npm link bower');
+        shell.exec('npm link less');
+        shell.exec('npm link coffee-script');
+        shell.exec('npm link to');
+        this.installDependencies({
+          callback: function () {
+            shell.exec('grunt bowercopy');
+            shell.exec('grunt initialize');
+          }
+        });
+      }
+      if (this.options['init']) {
+        this.installDependencies({
+          callback: function () {
+            shell.exec('grunt bowercopy');
+            shell.exec('grunt initialize');
+          }
+        });
+      }
+      if (!this.options['init'] || !this.options['install']) {
+        return;
       }
     });
   },
@@ -20,27 +41,41 @@ var ApolloGenerator = yeoman.generators.Base.extend({
     var done = this.async();
 
     // have Yeoman greet the user
-    console.log(this.yeoman);
+    if (this.options['welcome']) {
+      console.log(this.yeoman);
+    }
 
     // replace it with a short and sweet description of your generator
     console.log(chalk.magenta('You\'re using the fantastic Apollo generator.'));
 
     var prompts = [
-      {
-        type: 'confirm',
-        name: 'someOption',
-        message: 'Would you like to enable this option?',
-        default: true
-      },
+      // {
+      //   type: 'confirm',
+      //   name: 'someOption',
+      //   message: 'Would you like to enable this option?',
+      //   default: true
+      // },
       {
         name: 'projName',
         message: 'What is the name of the project?'
+      },
+      {
+        type: 'input',
+        name: 'projVers',
+        message: 'What is the version of the project?',
+        default: '0.0.1'
+      },
+      {
+        name: 'projDesc',
+        message: 'Give a description for the project:'
       }
     ];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      // this.someOption = props.someOption;
       this.projName   = props.projName;
+      this.projVers = props.projVers;
+      this.projDesc = props.projDesc;
 
       done();
     }.bind(this));
@@ -61,13 +96,17 @@ var ApolloGenerator = yeoman.generators.Base.extend({
   },
 
   root: function () {
-    this.template('_package.json', 'package.json');
+    if (this.options['install']) {
+      this.template('_package.json', 'package.json');
+    } else {
+      this.template('_package2.json', 'package.json');
+    }
+
     this.template('_bower.json', 'bower.json');
 
-    this.copy('Gruntfile.coffee', 'Gruntfile.coffee')
-    this.copy('init.coffee', 'init.coffee')
+    this.copy('Gruntfile.coffee', 'Gruntfile.coffee');
     this.copy('bowerrc', '.bowerrc');
-    this.copy('gitignore', '.gitignore')
+    this.copy('gitignore', '.gitignore');
     this.copy('editorconfig', '.editorconfig');
     this.copy('jshintrc', '.jshintrc');
   }
